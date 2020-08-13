c43cc47b04616e4d19e47b9832eca5a0382cd797
Alex Simpson <alxsimpson@gmail.com>
2020-08-13 14:30:09 +1000

    Dom manipulation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // $('h1#heading-one').hide();
 // $('.heading-two').hide();
 
-// $('p span').css('color', 'green');
+$('p span').css('color', 'green');
 
 $('ul#list li:first').css('color', 'blue');
 $('ul#list li:last').css('color', 'red');
@@ -96,4 +96,54 @@ $(document).ready(function() {
         console.log(name);
     });
 
+});
+
+// DOM MANIPULATION
+
+$(document).ready(function() {
+    $('p.paragraph-four').css({color: 'red', background: 'yellow'});
+    // $('p.paragraph-five').addClass('myClass');
+    // $('p.paragraph-five').removeClass('myClass');
+    $('#btn12').click(function() {
+        $('p.paragraph-five').toggleClass('myClass');
+    });
+
+    $('#myDiv').html('<h3>Hello World</h3>');
+    $('#myOtherDiv').text('<h3>Hello World</h3>');
+
+    $('ul#dom-list').append('<li>Appended Item</li>');
+    $('ul#dom-list').prepend('<li>Prepended Item</li>');
+
+    $('p.paragraph-four').appendTo('p.paragraph-five');
+    $('p.paragraph-four').prependTo('p.paragraph-five');
+    $('ul#dom-list').before('<h4>Hello</h4>');
+    $('ul#dom-list').after('<h4>World</h4>');
+    // $('ul#dom-list').empty();
+    // $('ul#dom-list').detach();
+    
+    $('a#yt').attr('target', '_blank');
+    // alert($('a#yt').attr('href'));
+    // $('a#yt').removeAttr('target');
+    
+    $('p.paragraph-four').wrap('<h1>');
+    $('ul#dom-list').wrapAll('<h3>');
+
+    $('#newItem').keyup(function(e){
+        var code = e.which;
+        if(code == 13){
+            e.preventDefault();
+            $('ul').append('<li>' + e.target.value + '</li>')
+        }
+    });
+
+    var myArr = ['Mason', 'Teller', 'Alice', 'Mike'];
+
+    $.each(myArr, function(i, val){
+        $('ul#users').append('<li>' + val + '</li>');
+    });
+
+    var newArr = $ ('ul#dom-list li').toArray();
+    $.each(newArr, function(i, val){
+        console.log(val.innerHTML);
+    })
 });
